ac8183833047bdb9c37cc32f9bc3b5000f67ccec
Emanuel-Motta-Silva <emanuel.motta@toodoo.com.br>
2021-12-20 11:29:15 -0500

    commit1

--- ComidaBoa/ViewController.swift
+++ ComidaBoa/ViewController.swift
@@ -6,14 +6,91 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    
+    var pedidos : [String] = []
+    
+    var ref:DatabaseReference?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnMalPassada(_ sender: UIButton) {
+        pedidos.append("Mal Passada")
+        ref?.child("Carne").childByAutoId().setValue("Mal Passada")
+    }
+    
+    @IBAction func btnAoPonto(_ sender: UIButton) {
+        pedidos.append("Ao Ponto")
+    }
+    
+    @IBAction func btnBemPassada(_ sender: UIButton) {
+        pedidos.append("Bem Passada")
+    }
+    
+    @IBAction func btnFrango(_ sender: UIButton) {
+        pedidos.append("Frango Assado")
+    }
+    
+    @IBAction func btnArrozBranco(_ sender: UIButton) {
+        pedidos.append("Arroz Branco")
+    }
+    
+    @IBAction func btnArrozIntegral(_ sender: UIButton) {
+        pedidos.append("Arroz Integral")
+    }
+    
+    @IBAction func btnTutu(_ sender: UIButton) {
+        pedidos.append("Tutu")
+    }
+    
+    @IBAction func btnFeijoada(_ sender: UIButton) {
+        pedidos.append("Feijoada")
+    }
+    
+    @IBAction func btnFeijaoCarioca(_ sender: UIButton) {
+        pedidos.append("Feijao Carioca")
+    }
+    
+    @IBAction func btnAlface(_ sender: UIButton) {
+        pedidos.append("Alface")
+    }
+    
+    @IBAction func btnBatata(_ sender: UIButton) {
+        pedidos.append("Salada de Batata")
+    }
+    
+    @IBAction func btnRucula(_ sender: UIButton) {
+        pedidos.append("Rucula")
+    }
+    
+    @IBAction func btnTomate(_ sender: UIButton) {
+        pedidos.append("Salada de Tomate")
+    }
+    
+    @IBAction func btnRocambole(_ sender: UIButton) {
+        pedidos.append("Rocambole")
+    }
+    
+    @IBAction func btnBatataFrita(_ sender: UIButton) {
+        pedidos.append("Batata Frita")
+    }
+    
+    @IBAction func btnQuiaboRefogado(_ sender: UIButton) {
+        pedidos.append("Quiabo Refogado")
+    }
+    
+    @IBAction func btnParm(_ sender: UIButton) {
+        pedidos.append("Parmegiana")
+    }
+    
+    @IBAction func btnFinalizar(_ sender: UIButton) {
+        print(pedidos)
+    }
 }
 
